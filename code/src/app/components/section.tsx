d24b4b43bc5d0dfdd4e62e8e9289b8b58e4e2fe4
prachinayak1509 <prachinayak1509@gmail.com>
2025-08-05 17:39:04 +0530

    scroll navigation

---
+++ code/src/app/components/section.tsx
@@ -0,0 +1,18 @@
+import Header from "./core/header"
+import { SectionContent } from "../models/sectionContent";
+export default function Section({ id, title, mainHeader, content }: SectionContent) {
+    return (
+
+        <section
+            id={id}
+            className="h-screen snap-start flex flex-col"
+        >
+            {mainHeader && <Header />}
+            <div className="flex-grow flex flex-col m-4">
+                <h1>{title}</h1>
+                <div>{content}</div>
+            </div>
+        </section>
+
+    );
+}
---
+++ code/src/app/models/sectionContent.ts
@@ -0,0 +1,11 @@
+
+import { type ReactElement } from "react";
+
+export interface SectionContent {
+  id: string;
+  title: string;
+  mainHeader?: boolean;
+  content: ReactElement;
+}
+
+
