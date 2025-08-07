f65d74f07c2144b3821c750d54a08fae7cdeefb1
prachinayak1509 <prachinayak1509@gmail.com>
2025-08-07 19:51:40 +0530

    header

--- code/src/app/landing/landing.tsx
+++ code/src/app/landing/landing.tsx
@@ -1,6 +1,9 @@
+import Header from "../components/core/header"
+
 export default function Landing() {
     return (
         <div>
+            <Header/>
             <h1>Landing</h1>
         </div>
     );
