9af8793f573b3119e49d5ec78128fa4b5035fa96
prachinayak1509 <prachinayak1509@gmail.com>
2025-08-14 01:02:19 +0530

    domains breakdown into smaller components

---
+++ code/src/app/domains/constants.ts
@@ -0,0 +1,22 @@
+export  const techDomains = [
+    [
+      { src: "/appdev.png", label: "App Dev" },
+      { src: "/webdev.png", label: "Web Dev" },
+    ],
+    [{ src: "/ai.png", label: "AI" }],
+    [
+      { src: "/cloud.png", label: "Cloud" },
+      { src: "/security.png", label: "Security" },
+    ],
+  ];
+
+  export const businessDomains = [
+    [
+      { src: "/charity.png", label: "Healthcare" },
+      { src: "/ecommerce.png", label: "Ecommerce" },
+    ],
+    [
+      { src: "/logistics.png", label: "Logistics" },
+      { src: "/marketplace.png", label: "Marketplace" },
+    ],
+  ];
