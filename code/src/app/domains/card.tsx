5580e68a48bb135eff514b5342fdc5afce3adaa6
Prachi Nayak <prachinayak1509@gmail.com>
2025-08-14 22:52:31 +0530

    Domains page (#10)

---
+++ code/src/app/domains/card.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+interface CardProps {
+  src: string;
+  label: string;
+}
+
+export default function Card({ src, label }: CardProps) {
+  return (
+    <div className="card flex flex-col items-center">
+      <Image src={src} width={50} height={50} alt={label} />
+      <span className="text-sm font-medium">{label}</span>
+    </div>
+  );
+}
--- code/src/app/domains/domains.tsx
+++ code/src/app/domains/domains.tsx
@@ -1,7 +1,11 @@
+import DomainsSection from "./domainSection";
+import { techDomains,businessDomains } from "./constants";
 export default function Domains() {
+
   return (
-    <div>
-      <h1>Domains</h1>
+    <div className="relative min-h-screen flex flex-col items-center justify-center">
+      <DomainsSection title="Tech Domains" items={techDomains} />
+      <DomainsSection title="Business Domains" items={businessDomains} />
     </div>
   );
 }
