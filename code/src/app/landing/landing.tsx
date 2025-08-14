1f43369cde5f76217f4e10e949a462066c79f855
Prachi Nayak <prachinayak1509@gmail.com>
2025-08-14 22:43:40 +0530

    Landing Page Animation (#8)

--- code/src/app/landing/landing.tsx
+++ code/src/app/landing/landing.tsx
@@ -1,10 +1,24 @@
+import Image from "next/image";
 import Header from "../components/core/header"
 
 export default function Landing() {
-    return (
-        <div>
-            <Header/>
-            <h1>Landing</h1>
-        </div>
-    );
+  return (
+    <div className="relative">
+      <Header />
+      <div className="m-4">
+        <h1>Landing page</h1>
+      </div>
+      <div className="absolute">
+        <Image
+          src="/globe.png"
+          width={350}
+          height={350}
+          alt="globe"
+          className="animate-spin [animation-duration:20s] [animation-timing-function:linear] [animation-iteration-count:infinite]"
+        />
+
+      </div>
+    </div>
+
+  );
 }
