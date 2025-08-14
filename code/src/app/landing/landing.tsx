fbe8426f4888b7762a0d06428ddce96f91e48dfb
Prachi Nayak <prachinayak1509@gmail.com>
2025-08-14 22:51:32 +0530

    Landing Page (#9)

--- code/src/app/landing/landing.tsx
+++ code/src/app/landing/landing.tsx
@@ -3,20 +3,33 @@ import Header from "../components/core/header"
 
 export default function Landing() {
   return (
-    <div className="relative">
+    <div className="relative min-h-screen overflow-hidden">
       <Header />
-      <div className="m-4">
-        <h1>Landing page</h1>
-      </div>
-      <div className="absolute">
+
+      <h1 className="absolute top-[15%] ">
+        AI-First Tech Services
+      </h1>
+
+      {/* <div className="absolute top-[10%] right-0 translate-x-1/3 translate-y-1/5 "> */}
+         <div className="absolute top-[20%] right-[-20%] md:right-[-10%] lg:right-[15%] lg:top-[10%] w-[90%] sm:w-[60%] md:w-[40%]">
         <Image
           src="/globe.png"
-          width={350}
-          height={350}
+          width={1000}
+          height={1000}
           alt="globe"
           className="animate-spin [animation-duration:20s] [animation-timing-function:linear] [animation-iteration-count:infinite]"
         />
+      </div>
 
+      <div className="absolute left-[10%] top-1/2 max-w-[35%]">
+        <h2 className="leading-tight">
+          Integrate AI across your stack and into your existing workflows
+        </h2>
+      </div>
+      <div className="absolute right-8 bottom-[10%] max-w-[30%]">
+        <h2 className="leading-tight">
+          our service provider network has got all your needs covered
+        </h2>
       </div>
     </div>
 
