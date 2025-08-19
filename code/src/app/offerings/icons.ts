382cd15aff3d141f92e6ae1d7ec4c6404f2f3757
prachinayak1509 <prachinayak1509@gmail.com>
2025-08-19 11:48:44 +0530

    offerings page

---
+++ code/src/app/offerings/icons.ts
@@ -0,0 +1,28 @@
+export const offeringItems = [
+  [
+    { src: "/consultation.png", label: "High Fidelity Planning" },
+    { src: "/draw.png", label: "Design-First" },
+  ],
+  [
+    { src: "/file-management.png", label: "Risk Driven Project Management" },
+    { src: "/marketplace.png", label: "Continuous Delivery" },
+  ],
+];
+export const techItems = [
+  "go.png", 
+  "phoenix.png", 
+  "python.png", 
+  "java-script.png",
+  "amazondynamodb.png", 
+  "mysql.png", 
+  "redis.png", 
+  "postgresql.png",
+  "aws.png", 
+  "rest-api.png", 
+  "kafka.png", 
+  "docker.png",
+  "gitlab.png", 
+  "sonarqube.png", 
+  "prometheus.png", 
+  "grafana.png"
+];
--- code/src/app/offerings/offerings.tsx
+++ code/src/app/offerings/offerings.tsx
@@ -1,7 +1,48 @@
+import Image from "next/image";
+import OfferingCard from "./card";
+import { offeringItems,techItems } from "./icons";
+
 export default function Offerings() {
-    return (
-        <div>
-            <h1>Offerings</h1>
+  return (
+    <div className="min-h-screen overflow-hidden flex flex-col items-center justify-center relative">
+
+      <h1 className="title">
+        Technologies we support
+      </h1>
+
+     <div className="grid grid-cols-4 sm:grid-cols-4 gap-8 lg:m-0">
+      {techItems.map((icon, idx) => (
+        <div key={idx} className="flex justify-center">
+          <Image
+            src={`/${icon}`}
+            alt={icon}
+            width={50}
+            height={50}
+          />
         </div>
-    );
+      ))}
+    </div>
+
+      {/* Section 2: Features */}
+      <div className=" flex flex-col items-center">
+        <h1 className="title mt-2">What makes us stand out</h1>
+        <div className="card-container flex flex-col gap-6 md:gap-0 lg:gap-0">
+          {offeringItems.map((row, idx) => (
+            <div
+              key={idx}
+              className={`grid ${row.length === 1
+                  ? "grid-cols-1 justify-center px-[30%]"
+                  : "grid-cols-2 gap-6"
+                }`}
+            >
+              {row.map((item) => (
+
+                <OfferingCard key={item.label} src={item.src} label={item.label} />
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
