ea076908150cb13864c81533fe25b5d5d2c1a34c
prachinayak1509 <prachinayak1509@gmail.com>
2025-08-20 23:00:45 +0530

    wip

--- code/next.config.ts
+++ code/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix: isProd ? '/smallTech-website' : '',
+  basePath: isProd ? '/smallTech-website' : '',
   images: { unoptimized: true },
 };
 
