b51004275b0b1283860a5c29b9220d33b76f174f
prachinayak1509 <prachinayak1509@gmail.com>
2025-08-25 13:26:32 +0530

    baspath removed

--- code/next.config.ts
+++ code/next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? '/smallTech-website' : '',
-  assetPrefix: isProd ? '/smallTech-website' : '',
   images: { unoptimized: true },
 };
 
