ab216eab7a461ac027f18e1b5a1bfe4a276dee7d
prachinayak1509 <prachinayak1509@gmail.com>
2025-08-28 12:13:31 +0530

    input field fix

--- code/src/app/chat/chat.tsx
+++ code/src/app/chat/chat.tsx
@@ -12,16 +12,21 @@ export default function Chat() {
   } = useChat();
 
   return (
-    <div className="flex flex-col w-full px-4 py-6 relative">
-      <h2 className="text-lg font-semibold mb-2 text-start">Meet Our AI Agent</h2>
-      <p className="text-sm mb-6 text-start">
+    <div className="flex flex-col w-full px-4 py-6 relative min-h-screen">
+      <h2 className="mb-2 text-start">Meet Our AI Agent</h2>
+      <p className="mb-6 text-start">
         Talk to our smart assistant to explore services
       </p>
 
 
+      <div className="flex flex-col text-xs items-center justify-center text-cream w-full mt-4 text-center font-thin">
+        <p className="w-50">contact@madhyamakist.com</p>
+        <p className="w-50 mt-5">© 2025 Madhyamakist Pvt. Ltd. All rights reserved.</p>
+      </div>
+
       <div
         ref={chatBoxText}
-        className="flex flex-col overflow-y-auto space-y-3 px-2 pb-4"
+        className="flex flex-col overflow-y-auto space-y-3 pb-4 ml-10"
         style={{
           flexGrow: 1,
           maxHeight: "calc(100vh - 220px)",
@@ -31,9 +36,9 @@ export default function Chat() {
         {messages.map((msg, i) => (
           <div
             key={i}
-            className={`max-w-[85%] px-3 py-2 rounded-lg text-sm ${msg.sender === "You"
-                ? "bg-brown self-end text-cinereous font-semibold"
-                : "bg-cinereous self-start text-brown font-semibold"
+            className={`max-w-[85%] px-3 py-2 rounded-lg ${msg.sender === "You"
+              ? "bg-brown self-end text-cinereous font-semibold"
+              : "bg-cinereous self-start text-brown font-semibold"
               }`}
           >
             {msg.text}
@@ -41,7 +46,31 @@ export default function Chat() {
         ))}
       </div>
 
+
       {isClient && (
+        <form
+          onSubmit={sendMessage}
+           className="mt-auto"
+        >
+          <div className="flex items-center bg-rose rounded-xl border border-black px-3 py-2 shadow-md">
+            <input
+              type="text"
+              className="flex-1 text-brown placeholder-brown bg-transparent outline-none"
+              placeholder="Type a message"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              required
+            />
+            <button
+              type="submit"
+              className="text-brown font-semibold ml-2 hover:text-cinereous transition"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      )}
+      {/* {isClient && (
         <form
           onSubmit={sendMessage}
           className="flex border border-2 border-black items-center w-full max-w-md bg-rose rounded-xl px-3 py-2"
@@ -61,7 +90,7 @@ export default function Chat() {
             Send
           </button>
         </form>
-      )}
+      )} */}
 
     </div>
   );
