ed7a6ddf3555a9b107333f0a946e7e676a994ea9
prachinayak1509 <prachinayak1509@gmail.com>
2025-09-03 17:01:37 +0530

    domains-mobile and tab

--- code/src/app/domains/domainSection.tsx
+++ code/src/app/domains/domainSection.tsx
@@ -7,19 +7,21 @@ interface DomainsSectionProps {
 
 export default function DomainsSection({ title, items }: DomainsSectionProps) {
   return (
-    <div className=" flex flex-col items-center">
-      <h1 className="title">{title}</h1>
-      <div className="card-container flex flex-col gap-6 md:gap-0 lg:gap-0 items-center justify-centerp ">
-        {items.map((row, idx) => (
-          <div
-            key={idx}
-            className={`grid ${row.length === 1 ? "grid-cols-1 justify-center px-[30%]" : "grid-cols-2 gap-6"}`}
-          >
-            {row.map((item) => (
-              <Card key={item.label} src={item.src} label={item.label} />
-            ))}
-          </div>
-        ))}
+    <div className="w-full flex flex-col">
+      <div className="w-full">
+        <h1 className="title">{title}</h1>
+        <div className="card-container flex flex-col">
+          {items.map((row, idx) => (
+            <div
+              key={idx}
+              className={`grid ${row.length === 1 ? "grid-cols-1 w-[30%] mx-auto place-items-center lg:w-[20%]" : "grid-cols-2 gap-[40%] lg:gap-[60%]"}`}
+            >
+              {row.map((item) => (
+                <Card key={item.label} src={item.src} label={item.label} />
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- code/src/app/domains/domains.tsx
+++ code/src/app/domains/domains.tsx
@@ -3,7 +3,7 @@ import { techDomains,businessDomains } from "./constants";
 export default function Domains() {
 
   return (
-    <div className="relative min-h-screen flex flex-col items-center justify-center">
+    <div className="relative min-h-screen w-full flex flex-col items-center pt-[10%] md:pt-[5%] gap-6">
       <DomainsSection title="Tech Domains" items={techDomains} />
       <DomainsSection title="Business Domains" items={businessDomains} />
     </div>
