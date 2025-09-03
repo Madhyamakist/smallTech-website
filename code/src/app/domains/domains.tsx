a3d1704aed905da80440a6b740f526386628a54a
prachinayak1509 <prachinayak1509@gmail.com>
2025-09-03 17:09:31 +0530

    domains: desktop

--- code/src/app/domains/domains.tsx
+++ code/src/app/domains/domains.tsx
@@ -3,7 +3,7 @@ import { techDomains,businessDomains } from "./constants";
 export default function Domains() {
 
   return (
-    <div className="relative min-h-screen w-full flex flex-col items-center pt-[10%] md:pt-[5%] gap-6">
+    <div className="relative min-h-screen w-full flex flex-col items-center pt-[10%] md:pt-[5%] lg:pt-0 gap-6">
       <DomainsSection title="Tech Domains" items={techDomains} />
       <DomainsSection title="Business Domains" items={businessDomains} />
     </div>
