639db445def44d3899c10188144ad989d213516d
prachinayak1509 <prachinayak1509@gmail.com>
2025-09-03 14:34:21 +0530

    footer

--- code/src/app/components/core/footer.tsx
+++ code/src/app/components/core/footer.tsx
@@ -1,17 +1,30 @@
 import Link from "next/link";
-
+import Image from "next/image";
 export default function Footer() {
     const currentYear = new Date().getFullYear();
     return (
-        <footer>
-            <div className="flex flex-col items-center gap-2">
-                <Link href="mailto:contact@madhyamakist.com" className="hover:underline">
-                    contact@madhyamakist.com
-                </Link>
-                <span>
-                    © {currentYear} Madyamakist Pvt Ltd. All rights reserved.
-                </span>
-            </div>
+        <footer className="flex items-center justify-center">
+             <div className="flex flex-col text-xs space-y-2 ">
+          <Link
+            href="https://blog.smallTech.in"
+            className="flex hover:underline space-x-2"
+          >
+            <Image src="./blog.png" width={15} height={15} alt="blog" />
+            <span className="text-white ">blog.smallTech.in</span>
+          </Link>
+          <Link
+            href="mailto:contact@madhyamakist.com"
+            className="flex items-center hover:underline space-x-2"
+          >
+            <Image src="./email.png" width={15} height={15} alt="email" />
+            <span className="text-white">contact@madhyamakist.com</span>
+          </Link>
+          
+          <div className="flex items-center space-x-2">
+            <Image src="./copyright.png" width={17} height={17} alt="blog" />
+            <span className="text-white">{currentYear} Madhyamakist Pvt Ltd</span>
+          </div>
+        </div>
 
         </footer>
     );
