47b2044bafa7e8d4b49f4b1882a5d5f887cd6626
Prachi Nayak <prachinayak1509@gmail.com>
2025-09-03 23:52:33 +0530

    Deploy (#16)

--- code/next.config.ts
+++ code/next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-    async rewrites() {
-    return [
-      {
-        source: "/api/:path*",   // frontend calls /api/chat
-        destination: "http://127.0.0.1:5000/:path*", // Flask backend
-      },
-    ];
-  },
   output: "export",
   images: { unoptimized: true },
 };
