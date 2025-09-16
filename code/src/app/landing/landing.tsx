09bbe5950ca0ed8c99e9b8e577ee11e0874dec18
prachinayak1509 <prachinayak1509@gmail.com>
2025-09-16 23:52:55 +0530

    credits

--- code/src/app/landing/landing.tsx
+++ code/src/app/landing/landing.tsx
@@ -11,7 +11,7 @@ export default function Landing() {
       </h1>
 
       {/* <div className="absolute top-[10%] right-0 translate-x-1/3 translate-y-1/5 "> */}
-         <div className="absolute top-[20%] right-[-20%] md:right-[-10%] lg:right-[15%] lg:top-[10%] w-[90%] sm:w-[60%] md:w-[40%]">
+      <div className="absolute top-[20%] right-[-20%] md:right-[-10%] lg:right-[15%] lg:top-[10%] w-[90%] sm:w-[60%] md:w-[40%]">
         <Image
           src="./globe.png"
           width={1000}
@@ -19,6 +19,27 @@ export default function Landing() {
           alt="globe"
           className="animate-spin [animation-duration:20s] [animation-timing-function:linear] [animation-iteration-count:infinite]"
         />
+         <div className="absolute right-[30%] text-white text-[10px]">
+      <a
+        href="https://www.freepik.com/author/hannazasimova"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="relative flex items-center group"
+      >
+        <Image
+          src="/credits.png"
+          width={16}
+          height={16}
+          alt="credits"
+          className="cursor-pointer"
+        />
+
+        {/* Tooltip */}
+        <span className="absolute bottom-full left-1/2 -translate-x-1/2 hidden group-hover:block text-white text-[5px]">
+         hannazasimova
+        </span>
+      </a>
+    </div>
       </div>
 
       <div className="absolute left-[5%] top-[60%] max-w-[35%]">
@@ -31,6 +52,27 @@ export default function Landing() {
           our service provider network has got all your needs covered
         </h2>
       </div>
+      <div className="absolute bottom-[0%] right-[0%] text-white text-[10px]">
+      <a
+        href="https://dribbble.com/abron"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="relative flex items-center group"
+      >
+        <Image
+          src="/credits.png"
+          width={16}
+          height={16}
+          alt="credits"
+          className="cursor-pointer"
+        />
+
+        {/* Tooltip */}
+        <span className="absolute bottom-full left-1/2 -translate-x-1/2 hidden group-hover:block  text-white text-[5px] ">
+         Abron Studio
+        </span>
+      </a>
+    </div>
     </div>
 
   );
