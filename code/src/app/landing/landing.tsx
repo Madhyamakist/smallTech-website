636b819531433eae2cf458223c2e59b9cc7e9544
Prachi Nayak <prachinayak1509@gmail.com>
2025-09-26 10:56:15 +0530

    Credits (#28)
    * credits
    
    * info icon
    
    * tooltip

--- code/src/app/landing/landing.tsx
+++ code/src/app/landing/landing.tsx
@@ -11,7 +11,7 @@ export default function Landing() {
       </h1>
 
       {/* <div className="absolute top-[10%] right-0 translate-x-1/3 translate-y-1/5 "> */}
-         <div className="absolute top-[20%] right-[-20%] md:right-[-10%] lg:right-[15%] lg:top-[10%] w-[90%] sm:w-[60%] md:w-[40%]">
+      <div className="absolute top-[20%] right-[-20%] md:right-[-10%] lg:right-[15%] lg:top-[10%] w-[90%] sm:w-[60%] md:w-[40%]">
         <Image
           src="./globe.png"
           width={1000}
@@ -19,6 +19,31 @@ export default function Landing() {
           alt="globe"
           className="animate-spin [animation-duration:20s] [animation-timing-function:linear] [animation-iteration-count:infinite]"
         />
+        <div className="absolute right-[30%] text-white text-[10px]">
+          <div className="tooltip">
+            <Image
+              src="/info.png"
+              width={16}
+              height={16}
+              alt="credits"
+              className="cursor-pointer"
+              tabIndex={0} // allows :focus on mobile
+            />
+
+            {/* Tooltip */}
+            <span className="tooltiptext">
+              <a
+                href="https://www.freepik.com/author/hannazasimova"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline"
+              >
+                hannazasimova
+              </a>
+            </span>
+          </div>
+        </div>
+
       </div>
 
       <div className="absolute left-[5%] top-[60%] max-w-[35%]">
@@ -30,6 +55,32 @@ export default function Landing() {
         <h2 className="leading-tight">
           our service provider network has got all your needs covered
         </h2>
+      </div>
+      <div className="absolute bottom-[0%] right-[0%] text-white text-[10px]">
+        <div className="tooltip">
+
+          <Image
+            src="/info.png"
+            width={16}
+            height={16}
+            alt="credits"
+            className="cursor-pointer"
+            tabIndex={0}
+          />
+
+          {/* Tooltip */}
+          <span className="tooltiptext ">
+            <a
+              href="https://dribbble.com/abron"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
+            >
+              Abron Studio
+
+            </a>
+          </span>
+        </div>
       </div>
     </div>
 
